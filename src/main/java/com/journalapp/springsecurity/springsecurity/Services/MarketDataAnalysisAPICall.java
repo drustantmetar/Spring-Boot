4a3d10edc9efe3eb372f5f6f97cdfa4c39888129
Drustant Metar <drustant.metar@gmail.com>
2026-04-12 18:37:46 +0530

    External POST request how to send it -- Code is commented only for hint it will not run but its just for hint how to do a POST call thrugh JAVA

--- src/main/java/com/journalapp/springsecurity/springsecurity/Services/MarketDataAnalysisAPICall.java
+++ src/main/java/com/journalapp/springsecurity/springsecurity/Services/MarketDataAnalysisAPICall.java
@@ -1,9 +1,11 @@
 package com.journalapp.springsecurity.springsecurity.Services;
 
+import com.journalapp.springsecurity.springsecurity.Model.users;
 import com.journalapp.springsecurity.springsecurity.api.response.MarketAnalysisResponse;
+import com.sun.net.httpserver.Headers;
+import io.jsonwebtoken.Header;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -24,7 +26,15 @@ public class MarketDataAnalysisAPICall {
         // To convert JSON response into the java object is call Deserialization
         ResponseEntity<MarketAnalysisResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, MarketAnalysisResponse.class);
 
-        return response.getBody();
+        // we can send also the post call to thrity party API
 
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.set("Key","Value");
+//        String a="";
+//        users user = users.builder().username("Drustant").password("Drustant");
+//        HttpEntity<users> httpEntity = new HttpEntity(user,headers);
+//        ResponseEntity<MarketAnalysisResponse> response1 = restTemplate.exchange(finalAPI,HttpMethod.POST,httpEntity,MarketAnalysisResponse.class);
+
+        return response.getBody();
     }
 }
