5a71a966a38a8669c16cbe618983dda7a20ba669
Drustant Metar <drustant.metar@gmail.com>
2026-04-30 01:03:30 +0530

    Addedthe Swagger Dpendencies and call the URL on browser

--- src/main/java/com/journalapp/springsecurity/springsecurity/Controller/HealthCheck.java
+++ src/main/java/com/journalapp/springsecurity/springsecurity/Controller/HealthCheck.java
@@ -1,10 +1,14 @@
 package com.journalapp.springsecurity.springsecurity.Controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "HEALTH CHECK API CALL")
 public class HealthCheck {
+    @Operation(summary = "Used to check the Health of the application")
     @GetMapping("/health-check")
     public String health() {
         return "Hello World";
